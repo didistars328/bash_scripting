333f11cdfe1b1eeb77880b9d7dcef1a42ec1ab8b
Denys Krytskyi <didistars328@gmail.com>
2020-03-06 23:00:05 +0200

    Add if running script

---
+++ if.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+RESTORE=$'\033[0m'
+
+RED=$'\033[00;31m'
+GREEN=$'\033[00;32m'
+YELLOW=$'\033[00;33m'
+BLUE=$'\033[00;34m'
+PURPLE=$'\033[00;35m'
+CYAN=$'\033[00;36m'
+LIGHTGRAY=$'\033[00;37m'
+
+LRED=$'\033[01;31m'
+LGREEN=$'\033[01;32m'
+LYELLOW=$'\033[01;33m'
+LBLUE=$'\033[01;34m'
+LPURPLE=$'\033[01;35m'
+LCYAN=$'\033[01;36m'
+WHITE=$'\033[01;37m'
+
+echo "${GREEN}==============="
+echo "##### IF ######"
+echo "===============${RESTORE}"
+echo "${YELLOW}Usage:${RESTORE}"
+cat <<-EOF
+if [ condition-true ]
+then
+  command 1
+  command 2
+  ...
+fi
+EOF
+echo "${BLUE}Example:${RESTORE}"
+if_function () {
+  cat <<EOF
+  MY_SHELL="bash"
+  if [ "$MY_SHELL" = "bash" ]
+  then
+    echo "You seem to like the bash shell."
+  fi
+EOF
+}
+if_function
+echo "${GREEN}TEST:${RESTORE}"
+eval "$(if_function)"
+echo
+
+echo "${GREEN}=================="
+echo "### IF # ELSE ####"
+echo "==================${RESTORE}"
+echo "${YELLOW}Usage:${RESTORE}"
+cat <<EOF
+if [ condition-true ]
+then
+  command 1
+  command 2
+  ...
+else
+  command 3
+  command 4
+  ...
+fi
+EOF
+echo "${BLUE}Example:${RESTORE}"
+if_else_function() {
+  cat <<EOF
+  MY_SHELL="zsh"
+  if [ "$MY_SHELL" = "bash" ]; then
+    echo "You seem to like the bash shell."
+  else
+    echo "You don't seem to like the bash shell."
+  fi
+EOF
+}
+if_else_function
+echo "${GREEN}TEST:${RESTORE}"
+eval "$(if_else_function)"
+echo
+
+echo "${GREEN}=================="
+echo "### IF # ELIF ####"
+echo "==================${RESTORE}"
+echo "${YELLOW}Usage:${RESTORE}"
+cat <<EOF
+if [ condition-true ]
+then
+  command 1
+  command 2
+  ...
+elif [ condition-true ]
+then
+  command 3
+  command 4
+  ...
+else
+  command 5
+  command 6
+  ...
+fi
+EOF
+if_elif_function() {
+  cat <<EOF
+  MY_SHELL="zsh"
+  if [ "$MY_SHELL" = "bash" ]
+  then
+    echo "You seem to like the bash shell."
+  elif [ "$MY_SHELL" = "zsh" ]
+  then
+    echo "You seem to like the zsh shell."
+  else
+    echo "You don't seem to like the bash or zsh shells."
+  fi
+EOF
+}
+echo "${BLUE}Example:${RESTORE}"
+if_elif_function
+echo "${GREEN}TEST:${RESTORE}"
+eval "$(if_elif_function)"
